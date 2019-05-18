8b3208bfb403b362dd3ac4fed7d5753e906dd1e1
AlexHladun <gefest.gl@gmail.com>
2019-05-18 15:40:52 +0300

    Solution

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('hello mate academy!');
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
+
+function createCalendar(id, year, month) {
+  const calendar = document.getElementById(id);
+  const table = document.createElement('table');
+  const header = document.createElement('tr');
+  const nameDays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  calendar.appendChild(table);
+  for (let i = 0; i < nameDays.length; i++) {
+    const th = document.createElement('th');
+    header.appendChild(th).innerHTML = nameDays[i];
+  };
+
+  table.appendChild(header);
+
+  const date = new Date(year, month - 1);
+  let firstDay = date.getDay();
+  firstDay = firstDay === 0 ? 6 : firstDay - 1;
+  let nextDayToAdd = 1 - firstDay;
+  const countDays = getDaysInMonth(year, month);
+
+  while (nextDayToAdd <= countDays) {
+    const week = document.createElement('tr');
+    for (let i = 0; i < 7; i++) {
+      const day = document.createElement('td');
+      if (nextDayToAdd > 0 && nextDayToAdd <= countDays) {
+        day.innerHTML = nextDayToAdd;
+      }
+      nextDayToAdd++;
+      week.append(day);
+    }
+    table.append(week);
+  }
+  return table;
+};
+createCalendar('calendar', 2012, 9);
